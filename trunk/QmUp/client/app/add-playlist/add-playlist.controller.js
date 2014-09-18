80acfa706decb99623d30a194a0f65f7b23235e6
oscha646 <oscha646@student.liu.se>
2014-09-18 12:57:58 +0200

    changed some files

--- trunk/QmUp/client/app/add-playlist/add-playlist.controller.js
+++ trunk/QmUp/client/app/add-playlist/add-playlist.controller.js
@@ -1,11 +1,13 @@
 'use strict';
 
 angular.module('qmUpApp')
-  .controller('AddPlaylistCtrl', function ($scope, $http, socket) {
+  .controller('AddPlaylistCtrl', function ($scope, $http, socket, Auth) {
     $scope.playlists = [];
     var counter =0;
+    $scope.loggedIn = Auth.isLoggedIn();
+    console.log(Auth.getCurrentUser());
 
-  $http.get('/api/playlists').success(function(response) {
+  $http.get('/api/playlists/user/'+Auth.getCurrentUser()._id).success(function(response) {
       			$scope.playlists = response;
       			console.log(response);
       			socket.syncUpdates('playlist', $scope.playlists);
@@ -13,13 +15,15 @@ angular.module('qmUpApp')
 
 
 
-  $scope.add = function (argument) {
+  $scope.add = function (name) {
   	counter++;
-  	var newTrack = {owner:"owner"+ counter, name: "name" + counter}
+  	var newTrack = {owner:{id: Auth.getCurrentUser()._id, name:Auth.getCurrentUser().name}, name: name};
+    console.log(newTrack);
 	$http.post('/api/playlists', newTrack ).success(function (argument) {
 		console.log(argument);
 
 	});
   	
+    
   }
   });
