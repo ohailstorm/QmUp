515db096a083183bb0bb187ea2dda2a5ef5d3870
oscha646 <oscha646@student.liu.se>
2014-09-18 10:00:20 +0200

    changed file structure

---
+++ legacy/api/application/models/Entities/Article.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Entities\Article
+ */
+class Article
+{
+    /**
+     * @var integer $id
+     */
+    private $id;
+
+    /**
+     * @var string $title
+     */
+    private $title;
+
+    /**
+     * @var text $content
+     */
+    private $content;
+
+    /**
+     * @var datetime $created
+     */
+    private $created;
+
+    /**
+     * @var Entities\User
+     */
+    private $user;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Article
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set content
+     *
+     * @param text $content
+     * @return Article
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return text 
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set created
+     *
+     * @param datetime $created
+     * @return Article
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return datetime 
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set user
+     *
+     * @param Entities\User $user
+     * @return Article
+     */
+    public function setUser(\Entities\User $user = null)
+    {
+        $this->user = $user;
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return Entities\User 
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
