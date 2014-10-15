41038ddaf660184f1e56fa280213f4082db4f376
Gustaf <gustaf@gustaf-VPCEB1M1E.(none)>
2014-10-15 13:23:06 +0200

    added msg if new playlist name is empty

--- trunk/QmUp/client/app/add-playlist/add-playlist.controller.js
+++ trunk/QmUp/client/app/add-playlist/add-playlist.controller.js
@@ -1,33 +1,61 @@
 'use strict';
 
 angular.module('qmUpApp')
-  .controller('AddPlaylistCtrl', function ($scope, $http, socket, Auth) {
-    $scope.playlists = [];
-    var counter =0;
-    $scope.loggedIn = Auth.isLoggedIn();
-    console.log(Auth.getCurrentUser());
+.controller('AddPlaylistCtrl', function ($scope, $http, socket, Auth) {
+  $scope.playlists = [];
+  var counter =0;
+  $scope.loggedIn = Auth.isLoggedIn();
+  console.log(Auth.getCurrentUser());
+  
+  //$scope.isError = false;
+  $scope.alerts = [
+  ];
+
+  $scope.addAlert = function(type,newMsg) {
+    $scope.alerts.push({type: type, msg: newMsg});
+  };
+
+  $scope.closeAlert = function(index) {
+    $scope.alerts.splice(index, 1);
+  };
+
 
   $http.get('/api/playlists/user/'+Auth.getCurrentUser()._id).success(function(response) {
-      			$scope.playlists = response;
-      			console.log(response);
+   $scope.playlists = response;
+   console.log(response);
       			//socket.syncUpdates('playlist', $scope.playlists);
-    			});
+         });
 
 
   $scope.add = function (newName) {
-  	counter++;
-    var own = Auth.getCurrentUser()._id;
-  	var newTrack = {name:newName, owner: own};
-    console.log(newTrack);
-	$http.post('/api/playlists', newTrack ).success(function (response) {
-		console.log(response);
-    if(response.owner===Auth.getCurrentUser()._id){
+    $scope.closeAlert();
+    if(newName !== undefined && newName.length > 0){
 
-    response.owner={_id:response.owner, name:Auth.getCurrentUser().name};
-    $scope.playlists.push(response);
-}
-	});    
+    
+    counter++;
+    var own = Auth.getCurrentUser()._id;
+    var newTrack = {name:newName, owner: own};
+    console.log(newTrack);
+    $http.post('/api/playlists', newTrack ).success(function (response) {
+      console.log(response);
+      if(response.owner===Auth.getCurrentUser()._id){
+
+        response.owner={_id:response.owner, name:Auth.getCurrentUser().name};
+        $scope.playlists.push(response);
+      }
+      $scope.addAlert('success',"Successfully created playlist");
+    });    
+  } else { 
+    console.log("Unnamed playlist");
+    
+    $scope.addAlert('danger', "Unnamed playlist");
+    
   };
+};
+
+  $scope.lookForPlaylist = function (name) {
+
+  }
 
 
   $scope.addCollab = function (playlist) {
@@ -35,10 +63,10 @@ angular.module('qmUpApp')
     var postObject =  {user: Auth.getCurrentUser()._id};
 
     $http.post('/api/playlists/'+playlist._id+'/collaborator', postObject).success(function(response) {
-            $scope.playlists = response;
-            console.log(response);
-            socket.syncUpdates('playlist', $scope.playlists);
-          });
+      $scope.playlists = response;
+      console.log(response);
+      socket.syncUpdates('playlist', $scope.playlists);
+    });
 
   };
-  });
+});
--- trunk/QmUp/client/components/navbar/navbar.controller.js
+++ trunk/QmUp/client/components/navbar/navbar.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('qmUpApp')
-  .controller('NavbarCtrl', function ($scope, $location, Auth) {
+  .controller('NavbarCtrl', function ($scope, $location, Auth, $window) {
     $scope.menu = [{
       'title': 'Home',
       'link': '/'
@@ -20,6 +20,10 @@ angular.module('qmUpApp')
     $scope.isLoggedIn = Auth.isLoggedIn;
     $scope.isAdmin = Auth.isAdmin;
     $scope.getCurrentUser = Auth.getCurrentUser;
+
+    $scope.loginOauth = function(provider) {
+      $window.location.href = '/auth/' + provider;
+    };
 
     $scope.logout = function() {
       Auth.logout();
